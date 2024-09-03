00a4b9c62762e5c2684703ab06c10eac4a38a639
alfa38 <alpharium89@gmail.com>
2024-09-03 12:49:36 +0300

    # deleted junk content-types

--- src/api/custom-upload/services/custom-upload.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-/**
- * custom-upload service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::custom-upload.custom-upload');
--- src/api/product/services/calculate.ts
+++
@@ -1,9 +0,0 @@
-/**
- * calculate service
- */
-
-export default () => ({
-  calculatePlus(number1, number2) {
-    return number1 + number2;
-  }
-});
