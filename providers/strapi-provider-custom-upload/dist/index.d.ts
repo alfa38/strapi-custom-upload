e9949b8ae57f3cffc4b3cedff3828584087939b9
alfa38 <alpharium89@gmail.com>
2024-09-10 08:54:58 +0300

    provider dist

--- providers/strapi-provider-custom-upload/dist/index.d.ts
+++ providers/strapi-provider-custom-upload/dist/index.d.ts
@@ -1,42 +1,12 @@
 /// <reference types="node" />
-/// <reference types="node" />
-import { ReadStream } from 'fs';
-interface File {
-    name: string;
-    alternativeText?: string;
-    caption?: string;
-    width?: number;
-    height?: number;
-    formats?: Record<string, unknown>;
-    hash: string;
-    ext?: string;
-    mime: string;
-    size: number;
-    sizeInBytes: number;
-    url: string;
-    previewUrl?: string;
-    path?: string;
-    provider?: string;
-    provider_metadata?: Record<string, unknown>;
-    stream?: ReadStream;
-    buffer?: Buffer;
-    folderPath?: string;
-}
-interface InitOptions {
-    sizeLimit?: number;
-    rootPath?: string;
-    uploadDir?: string;
-}
-interface CheckFileSizeOptions {
-    sizeLimit?: number;
-}
+import { CheckFileSizeOptions, IFile, InitOptions } from './types';
 declare const _default: {
-    init({ sizeLimit: providerOptionsSizeLimit, rootPath, uploadDir }?: InitOptions): {
+    init({ sizeLimit: providerOptionsSizeLimit }?: InitOptions): {
         checkFileSize(file: File, options: CheckFileSizeOptions): void;
-        uploadStream(file: File): Promise<void>;
-        upload(file: File): Promise<void>;
+        uploadStream(file: IFile): Promise<void>;
+        upload(file: IFile): Promise<void>;
         isPrivate(): boolean;
-        delete(file: File): Promise<string | void>;
+        delete(file: IFile): Promise<string | void>;
     };
 };
 export = _default;
--- providers/strapi-provider-custom-upload/dist/index.js
+++ providers/strapi-provider-custom-upload/dist/index.js
@@ -26,27 +26,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const stream_1 = require("stream");
-const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const fs_extra_1 = __importDefault(require("fs-extra"));
 const utils = __importStar(require("@strapi/utils"));
 const { PayloadTooLargeError } = utils.errors;
 const { kbytesToBytes, bytesToHumanReadable } = utils.file;
 const UPLOADS_FOLDER_NAME = 'uploads';
-const ensureDirectory = async (uploadPath) => {
+const ensureDirectory = (uploadPath) => {
     const options = {
         mode: 0o2777 // crash with 0o2775
     };
     fs_extra_1.default.ensureDirSync(uploadPath, options);
 };
+const findFirstEntry = async (pluginId, query) => {
+    const entries = await strapi.entityService.findMany(pluginId, query);
+    if (!entries)
+        return {};
+    if (Array.isArray(entries)) {
+        if (entries.length === 0) {
+            return {};
+        }
+        else {
+            return entries[0];
+        }
+    }
+};
 const getCurrentTarget = async () => {
-    const { selection } = await strapi.db.query('plugin::custom-upload.target-link').findOne();
+    const selection = await findFirstEntry('plugin::custom-upload.target-link');
     return selection;
 };
 const getCurrentUploadPath = async () => {
     const root = strapi.dirs.app.root;
     const currentTarget = await getCurrentTarget();
-    const { link } = await strapi.db.query('plugin::custom-upload.custom-upload').findOne({ where: { name: currentTarget } });
+    const { link } = await findFirstEntry('plugin::custom-upload.custom-upload', { where: { name: currentTarget } });
     const uploadPath = path_1.default.resolve(`${root}/files/${link}`);
     if (!fs_extra_1.default.pathExistsSync(uploadPath)) {
         ensureDirectory(uploadPath);
@@ -54,15 +66,16 @@ const getCurrentUploadPath = async () => {
     return { uploadPath, link };
 };
 const getUploadPathFromFile = (file) => {
-    if (file.url.startsWith('public')) {
+    const url = file.url.split('?')[0];
+    if (url.startsWith('public')) {
         return `${strapi.dirs.app.root}\\public\\${UPLOADS_FOLDER_NAME}\\${file.hash}${file.ext}`;
     }
-    const filePath = file.url.split('/').splice(6).join('\\').split('?')[0];
+    // const filePath = file.url.split('/').splice(2).join('\\').split('?')[0];
+    const filePath = url.split('/').splice(2).reduce((prevValue, nextValue) => `${prevValue}/${nextValue}`);
     return `${strapi.dirs.app.root}\\files\\${filePath}`;
 };
 module.exports = {
-    init({ sizeLimit: providerOptionsSizeLimit, rootPath, uploadDir } = {}) {
-        // TODO V5: remove providerOptions sizeLimit
+    init({ sizeLimit: providerOptionsSizeLimit } = {}) {
         if (providerOptionsSizeLimit) {
             process.emitWarning('[deprecated] In future versions, "sizeLimit" argument will be ignored from upload.config.providerOptions. Move it to upload.config');
         }
@@ -86,17 +99,11 @@ module.exports = {
                 const { stream } = file;
                 return new Promise(async (resolve, reject) => {
                     const { uploadPath, link } = await getCurrentUploadPath();
-                    (0, stream_1.pipeline)(stream, fs_1.default.createWriteStream(path_1.default.join(uploadPath, `${file.hash}${file.ext}`)), (err) => {
+                    (0, stream_1.pipeline)(stream, fs_extra_1.default.createWriteStream(path_1.default.join(uploadPath, `${file.hash}${file.ext}`)), (err) => {
                         if (err) {
                             return reject(err);
                         }
-                        // file.url = `/${rootPath ?? 'public'}${uploadDir ?? UPLOADS_FOLDER_NAME}/${file.hash}${file.ext}`;
-                        if (!rootPath) {
-                            file.url = `public/${uploadDir ?? UPLOADS_FOLDER_NAME}/${file.hash}${file.ext}`;
-                        }
-                        else {
-                            file.url = `http://localhost:1337/api/root-files/files${link}/${file.hash}${file.ext}`;
-                        }
+                        file.url = `custom-upload/root-files${link.startsWith('/') ? link : `/${link}`}/${file.hash}${file.ext}`;
                         resolve();
                     });
                 });
@@ -108,19 +115,12 @@ module.exports = {
                 const { buffer } = file;
                 return new Promise(async (resolve, reject) => {
                     const { uploadPath, link } = await getCurrentUploadPath();
-                    console.log('upload', uploadPath);
                     // write file in public/assets folder
-                    fs_1.default.writeFile(path_1.default.join(uploadPath, `${file.hash}${file.ext}`), buffer, (err) => {
+                    fs_extra_1.default.writeFile(path_1.default.join(uploadPath, `${file.hash}${file.ext}`), buffer, (err) => {
                         if (err) {
                             return reject(err);
                         }
-                        // file.url = `/${rootPath ?? 'public'}${uploadDir ?? UPLOADS_FOLDER_NAME}/${file.hash}${file.ext}`;
-                        if (!rootPath) {
-                            file.url = `public/${uploadDir ?? UPLOADS_FOLDER_NAME}/${file.hash}${file.ext}`;
-                        }
-                        else {
-                            file.url = `http://localhost:1337/api/root-path/files${link}/${file.hash}${file.ext}`;
-                        }
+                        file.url = `custom-upload/root-files${link.startsWith('/') ? link : `/${link}`}/${file.hash}${file.ext}`;
                         resolve();
                     });
                 });
@@ -131,14 +131,14 @@ module.exports = {
             delete(file) {
                 return new Promise(async (resolve, reject) => {
                     const uploadPath = getUploadPathFromFile(file);
-                    // const filePath = path.join(uploadPath, `${file.hash}${file.ext}`);
                     const filePath = uploadPath;
-                    if (!fs_1.default.existsSync(filePath)) {
+                    console.log(filePath);
+                    if (!fs_extra_1.default.existsSync(filePath)) {
                         resolve("File doesn't exist");
                         return;
                     }
-                    // remove file from public/assets folder
-                    fs_1.default.unlink(filePath, (err) => {
+                    // remove file from the path
+                    fs_extra_1.default.unlink(filePath, (err) => {
                         if (err) {
                             return reject(err);
                         }
